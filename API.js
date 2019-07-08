59fbe18ba7bcb400c9c9b02f88d8477b99e89340
Bren <captainbmq@gmail.com>
2019-07-08 10:05:17 -0400

    added database link

--- API.js
+++ API.js
@@ -1,14 +1,22 @@
 // call package and storing it
 const express = require('express');
+const mysql      = require('mysql');
 
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'avacado1219'
+  });
+  
+db.connect(function(err){
+    if(err) {throw err}
+    console.log("gucci gang")
+});
+  
 // call express and storing it
+
 const api = express();
-
-
-
-
-
-
 
 // app is listening on port 5000 
 api.listen(5000);
