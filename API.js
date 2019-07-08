16298005a0b594471b6ec8ed70f68d9d2911e8b5
Bren <captainbmq@gmail.com>
2019-07-08 09:39:12 -0400

    spicy first init

---
+++ API.js
@@ -0,0 +1,17 @@
+// call package and storing it
+const express = require('express');
+
+// call express and storing it
+const api = express();
+
+
+
+
+
+
+
+// app is listening on port 5000 
+api.listen(5000);
+
+// server start up message
+console.log("you have the big gay of 5000...")
