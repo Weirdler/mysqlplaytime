a1e857f42ae44a7629ef57a131946a3079c9f6f7
Bren <captainbmq@gmail.com>
2019-07-08 13:31:33 -0400

    messed with databases again

--- API.js
+++ API.js
@@ -6,7 +6,8 @@ const mysql      = require('mysql');
 const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'admin',
-  password : 'avacado1219'
+  password : 'avacado1219',
+  database : 'blog'
   });
   
 db.connect(function(err){
@@ -15,8 +16,77 @@ db.connect(function(err){
 });
   
 // call express and storing it
-
 const api = express();
+
+// creating database
+api.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE blog'
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database was created');
+    });
+});
+
+api.get('/createtb', (req, res) => {
+    let sql = "CREATE TABLE posts ( ID int NOT NULL AUTO_INCREMENT, Title varchar(255), Body varchar(255), PRIMARY KEY (ID))"
+    db.query(sql,(err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('table created')
+    })
+});
+
+api.get('/addpost', (req, res) => {
+    let post = {title: "spicy time", body: "lorem ipsum abdor sucam ajslkf skd fsljfkjaeoi kasdhfs jfasjdfl faoihvn hasdfdvjk"};
+    let sql = "INSERT INTO posts SET ?";
+    db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('table data added')
+    })
+
+});
+
+api.get('/add2ndpost', (req, res) => {
+    let post = {title: "Gucci", body: "spaghettie"};
+    let sql = "INSERT INTO posts SET ?";
+    db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('table data added')
+    })
+
+});
+api.get('/getposts', (req, res) => {
+    let sql = "select * from posts";
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result)
+    })
+});
+
+api.get('/getpost/:id', (req, res) => {
+    let sql = "select * from posts WHERE ID =" + req.params.id;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(req.params)
+        res.send('table data selected')
+    })
+
+});
+api.get('/deletepost/:id', (req, res) => {
+    let sql = "DELETE FROM posts WHERE ID =" + req.params.id;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('table data deleted')
+    })
+
+});
+
 
 // app is listening on port 5000 
 api.listen(5000);
